3031cd5be2fc180b228208586f4012fcc18472a1
EduardEgcho <egiazaryan-eduard@example.com>
2024-05-03 19:23:37 +0300

    flash bag

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TodosPage from "./pages/TodosPage";
+import './App.css'
+
+function App() {
+    return (
+        <div>
+            <TodosPage/>
+        </div>
+    );
+}
+
+export default App;
+
+
+
+
+
+
